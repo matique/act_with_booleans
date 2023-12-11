1865d350a03aaa9cef257eae635e33081c77bda9
dittmar <dittmar.krall@matique.de>
2023-12-11 10:59:10 +0100

    'v0.0.4'

--- lib/act_with_booleans/version.rb
+++ lib/act_with_booleans/version.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 module ActWithBooleans
-  VERSION = "0.0.3" # 2023-04-23
+  VERSION = "0.0.4" # 2023-12-11
+  # VERSION = "0.0.3" # 2023-04-23
   # VERSION = "0.0.2" # 2022-12-11
   # VERSION = "0.0.1" # 2022-09-10
 end
